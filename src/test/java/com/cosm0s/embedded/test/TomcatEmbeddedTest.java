fe9d1e1cfa75624f253e1c905ad5196463f915b0
apasc <apascualco@gmail.com>
2016-08-12 21:41:42 +0200

    add selenium test

--- src/test/java/com/cosm0s/embedded/test/TomcatEmbeddedTest.java
+++ src/test/java/com/cosm0s/embedded/test/TomcatEmbeddedTest.java
@@ -6,10 +6,16 @@ import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+
+import static org.junit.Assert.assertTrue;
 
 public class TomcatEmbeddedTest {
 
@@ -29,9 +35,11 @@ public class TomcatEmbeddedTest {
     public static void end() throws LifecycleException {
         TomcatEmbedded.stopTomcat();
     }
-
     @Test
     public void test_tomcat_embedded() {
-        while(true){}
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://localhost:8080/showcase-6.0/");
+        WebElement element = driver.findElement(By.id("LOGOTEXTSIDE")).findElement(By.className("logoBlueText"));
+        assertTrue("Showcase is down", element.getText().equals("SHOWCASE"));
     }
 }
